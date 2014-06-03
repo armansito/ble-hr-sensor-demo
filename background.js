80eb6880680e533b828fc5f142d984614cf61241
Arman Uguray <armansito@chromium.org>
2014-06-02 17:16:26 -0700

    Resized minimum window bounds.

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
     'bounds': {
-      'width': 500,
-      'height': 300,
+      'width': 410,
+      'height': 230,
       'left': 100,
       'top': 100
     },
-    minWidth: 500,
-    minHeight: 300
+    minWidth: 410,
+    minHeight: 230
   });
 });
